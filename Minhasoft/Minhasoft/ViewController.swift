9b8bbd0005e5c3ed3378ece64a7ad24c968df7bf
Rajesh <rajesh@Rajeshs-Mac.local>
2019-02-05 22:39:48 -0800

    first commit

---
+++ Minhasoft/Minhasoft/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Minhasoft
+//
+//  Created by Rajesh on 05/02/2019.
+//  Copyright © 2019 Rajesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
